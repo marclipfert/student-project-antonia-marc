72376f2082291680d8993e909b957c3138246190
antoniaen <49433755+antoniaen@users.noreply.github.com>
2019-07-29 10:57:18 +0200

    Update travis_runner.py

--- utils/travis_runner.py
+++ utils/travis_runner.py
@@ -4,6 +4,6 @@ import subprocess as sp
 
 if __name__ == '__main__':
 
-    notebook = 'student_project.ipynb'
+    notebook = 'student_project_Antonia_Marc.ipynb'
     cmd = ' jupyter nbconvert --execute {}  --ExecutePreprocessor.timeout=-1'.format(notebook)
     sp.check_call(cmd, shell=True)
